b9eb0730fccf81333ec2716397947331ea97e994
Connor Francey <connorfranceydev@gmail.com>
2026-07-07 04:58:12 -0600

    Spritesheets

--- src/main/java/com/openglgame/engine/util/AssetPool.java
+++ src/main/java/com/openglgame/engine/util/AssetPool.java
@@ -1,5 +1,6 @@
 package com.openglgame.engine.util;
 
+import com.openglgame.engine.components.Spritesheet;
 import com.openglgame.engine.renderer.Shader;
 import com.openglgame.engine.renderer.Texture;
 import org.apache.logging.log4j.LogManager;
@@ -10,10 +11,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AssetPool {
+    // Logger
+    private static Logger LOGGER = LogManager.getLogger(AssetPool.class);
+
     private static Map<String, Shader> shaders = new HashMap<>();
     private static Map<String, Texture> textures = new HashMap<>();
+    private static Map<String, Spritesheet> spritesheets = new HashMap<>();
 
     public static Shader getShader(String resourceName) {
+        LOGGER.info("Getting shader from resource: {}", resourceName);
         File file = new File(resourceName);
         if (AssetPool.shaders.containsKey(file.getAbsolutePath())) {
             return AssetPool.shaders.get(file.getAbsolutePath());
@@ -26,6 +32,7 @@ public class AssetPool {
     }
 
     public static Texture getTexture(String resourceName) {
+        LOGGER.info("Getting texture from resource: {}", resourceName);
         File file = new File(resourceName);
         if (AssetPool.textures.containsKey(file.getAbsolutePath())) {
             return AssetPool.textures.get(file.getAbsolutePath());
@@ -34,5 +41,21 @@ public class AssetPool {
             AssetPool.textures.put(file.getAbsolutePath(), texture);
             return texture;
         }
+    }
+
+    public static void addSpritesheet(String resourceName, Spritesheet spritesheet) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            AssetPool.spritesheets.put(file.getAbsolutePath(), spritesheet);
+        }
+    }
+
+    public static Spritesheet getSpritesheet(String resourceName) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            LOGGER.error("Tried to access spritesheet '{}' and it has not been added to asset pool.");
+            assert false : "Error: Tried to access spritesheet '" + resourceName + "' and it has not been added to asset pool.";
+        }
+        return AssetPool.spritesheets.getOrDefault(file.getAbsolutePath(), null);
     }
 }
